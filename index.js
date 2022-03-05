4ecc5e8a1a7d1772fdf6e839816739b15c71fa06
Ali Parvizi <snparvizi75@gmail.com>
2022-03-05 03:34:55 -0800

    decode input notification p2wpkh/p2pkh

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ const bitcoin = __importStar(require("bitcoinjs-lib"));
 const bs58check_ts_1 = __importDefault(require("bs58check-ts"));
 const crypto = __importStar(require("crypto"));
 const xor_1 = require("./xor");
+const txHexDecoder = require("raw-transaction-hex-decoder");
 class BIP47 {
     static fromPaymentCode(paymentCode, network = network_data_mainnet) {
         let bip47 = new BIP47();
@@ -153,36 +154,40 @@ class BIP47 {
         binaryPaymentCode.fill((0, xor_1.xor)(s.slice(32, 64), binaryPaymentCode.slice(35, 67)), 35, 67);
         return binaryPaymentCode.toString('hex');
     }
-    parseNotificationIn(notfData) {
-        // look for signature script, or in the redeem script or pubkey script
-        let tx = bitcoin.Transaction.fromHex(notfData);
-        let txid = tx.getId();
-        let inputs = tx.ins;
-        if (inputs[0].witness.length != 0) {
-            let input = inputs[0];
-            // retrieve pub key from witness
-            let A = inputs[0].witness[1];
-            let b = this.getNotificationNode().privateKey;
-            let S = BIP47.uintArrayToBuffer(ecc.pointMultiply(A, b));
-            let outpoint = Buffer.alloc(36);
-            outpoint[35] = 0x00;
-            outpoint[34] = 0x00;
-            outpoint[33] = 0x00;
-            outpoint[32] = 0x00;
-            for (let i = 0; i < 32; i++)
-                outpoint[i] = input.hash[i];
-            let x = BIP47.uintArrayToBuffer(ecc.xOnlyPointFromPoint(S));
-            let _hmac = crypto.createHmac('sha512', outpoint);
-            let s = _hmac.update(x).digest();
-            let outputs = tx.outs;
-            let binaryPaymentCode = outputs[0].script.slice(3);
-            binaryPaymentCode.fill((0, xor_1.xor)(s.slice(0, 32), binaryPaymentCode.slice(3, 35)), 3, 35);
-            binaryPaymentCode.fill((0, xor_1.xor)(s.slice(32, 64), binaryPaymentCode.slice(35, 67)), 35, 67);
-            console.log(bs58check_ts_1.default.encode(Buffer.concat([Buffer.from([71]), binaryPaymentCode])));
-        }
-        else {
-            // retrieve pub key from script
-        }
+    getFirstExposedPubKeyAndOutpoint(tx) {
+        const first = tx.ins[0];
+        const hash = first.hash;
+        const index = first.index;
+        const indexHex = index.toString(16).padStart(8, '0');
+        const outpoint = Buffer.from(hash.toString('hex') + indexHex, 'hex');
+        let pubKey = null;
+        if (first.witness.length)
+            pubKey = first.witness[1];
+        else if (bitcoin.script.toASM(first.script).split(' ').length == 2)
+            pubKey = Buffer.from(bitcoin.script.toASM(first.script).split(' ')[1], 'hex');
+        else
+            throw "Unknown Transaction type";
+        return {
+            'outpoint': outpoint,
+            'pubKey': pubKey
+        };
+    }
+    getPaymentCodeFromRawNotificationTransaction(rawHexNotificationData) {
+        let tx = bitcoin.Transaction.fromHex(rawHexNotificationData);
+        let { pubKey, outpoint } = this.getFirstExposedPubKeyAndOutpoint(tx);
+        let A = pubKey;
+        let b = this.getNotificationNode().privateKey;
+        let S = BIP47.uintArrayToBuffer(ecc.pointMultiply(A, b));
+        let x = BIP47.uintArrayToBuffer(ecc.xOnlyPointFromPoint(S));
+        let _hmac = crypto.createHmac('sha512', outpoint);
+        let s = _hmac.update(x).digest();
+        let opReturnOutput = tx.outs.find((o) => o.script.toString('hex').startsWith('6a4c50'));
+        if (!opReturnOutput)
+            throw "No OP_RETURN output in notification";
+        let binaryPaymentCode = opReturnOutput.script.slice(3);
+        binaryPaymentCode.fill((0, xor_1.xor)(s.slice(0, 32), binaryPaymentCode.slice(3, 35)), 3, 35);
+        binaryPaymentCode.fill((0, xor_1.xor)(s.slice(32, 64), binaryPaymentCode.slice(35, 67)), 35, 67);
+        console.log(bs58check_ts_1.default.encode(Buffer.concat([Buffer.from([71]), binaryPaymentCode])));
     }
 }
 BIP47.G = Buffer.from("0279BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798", 'hex');
@@ -205,6 +210,8 @@ console.assert(bobBIP47.getNotificationNode().publicKey.toString('hex') == "024c
 aliceBIP47.getNotificationOut(bobBIP47);
 let t = BIP47.fromBIP39Seed("sure plug leisure member give dress grain music embody able isolate curious", network_data_testnet);
 // console.log(t.getNotificationNode().toWIF())
+let testHex = "02000000024aac0078e3cccc900c35a10b5029e53084e9e762590e9d4aa8aa598210355310010000006a473044022030c7445ca81232ec5935f72827cd39b6e3b134d92be5853562bcc01f61e2bfde02205b45029d840fb6d1a912fcb9dd031bbaa305607dd066f0fcc8098b7253a3c0f3012103fdea480e61173609facb4a5b32f216b954e669adeacb1525da4dd4adfb7507f1fdffffff066d5d7e8a0a4482262fa14860e0d3dc8ef84b8ff1a09cd050185a8ec5ed9c77010000006a47304402204695ae58d86696d3372fac1937815da7f28a3c2e2a1ac28860034895ff28f74f02203d881ec0c72554fe72d2ad4fd959589bb2bb047e1dde9e8252a4933f629c9011012103fdea480e61173609facb4a5b32f216b954e669adeacb1525da4dd4adfb7507f1fdffffff01f0020000000000001976a914be908596e5ff768781a2f6842b5b1a56f94373ab88acf6082100";
 let rawP2PKHNotification = "0200000001544fa314c2d62077ae08b73f67cfe3b1bbffb5d58c816e35b932d2836d6a3bd6000000006a47304402202ee0f1e056d212f6a162661fd69ca62508b9c1c3f8d39f2c072ffe9cb39ab20e0220618db06b730d708440530dd076cda511c6619e85c7477ad42ebd3b51a2a25cc6012103b93ea930391aab97f03490cfa7a52d219430a02792e2abc66fead006a6de049cffffffff040000000000000000536a4c500100038b526ece105975c73d6d7787e5d5cf25a7f79bdd6e13b4faf2e9d157b03b67de8db542a92e035670c01f2934cae01d95541eba1c5bb3ed81db3c9e89439daa810000000000000000000000000022020000000000001976a91465097af8412ccaa542421643a9a97300a4f91e8f88ac983a0000000000001600144411eaf4b9ce4367e1fa29c6c42f259eff94e43956480100000000001600144b47d20f600b0510bd4f62f6d4a80eb732d3f8cb00000000";
 let rawP2WPKHNotification = "02000000000101e4cecfc286136cabb8c4a03baba7f549a20c748f065417da52722995c6e442d00000000000ffffffff040000000000000000536a4c50010003dbcc9c4d0932c85a86856fad2b45c190b40f1b265f8959544d08bd827b817cbf1db2c5c779fb9d2ad0c4796c001a95d1d7358713acdbbfd1826df8578a16ec900000000000000000000000000022020000000000001976a91465097af8412ccaa542421643a9a97300a4f91e8f88ac983a0000000000001600145761016026197c054f46e4a5f10e8fe923349e94f73d030000000000160014edc72cff2800d3c2a605cdcf0b45bb766e519bfb0247304402205d548a0042ac87a7a0102f46dc9491641cb34b22e9ef02020242972580dae56d022043ee0114beec3203ff2b303813d8e8174d3c69ecfdc6d093c54a186a84e90222012102d659fda36c83fb7fe09f4151ccd0e48531dfd26d998aeda3e6132c318db3006900000000";
-t.parseNotificationIn(rawP2WPKHNotification);
+t.getPaymentCodeFromRawNotificationTransaction(rawP2WPKHNotification);
+t.getPaymentCodeFromRawNotificationTransaction(rawP2PKHNotification);
